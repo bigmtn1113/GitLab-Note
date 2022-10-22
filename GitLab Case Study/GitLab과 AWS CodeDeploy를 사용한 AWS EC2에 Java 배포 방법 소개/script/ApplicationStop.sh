4aefa339b9b0aebbdc91c63e103f1d0e36f87298
kva231 <kva231@naver.com>
2022-10-22 19:26:09 +0900

    Update files

--- GitLab Case Study/GitLab과 AWS CodeDeploy를 사용한 AWS EC2에 Java 배포 방법 소개/script/ApplicationStop.sh
+++ GitLab Case Study/GitLab과 AWS CodeDeploy를 사용한 AWS EC2에 Java 배포 방법 소개/script/ApplicationStop.sh
@@ -6,4 +6,3 @@ sudo killall java
 if [ -d /spring-boot-helloworld/release ]; then
     sudo rm -rf /spring-boot-helloworld/release
 fi
-sudo mkdir -vp /spring-boot-helloworld/release
--- GitLab Case Study/GitLab과 AWS CodeDeploy를 사용한 AWS EC2에 Java 배포 방법 소개/script/BeforInstall.sh
+++ GitLab Case Study/GitLab과 AWS CodeDeploy를 사용한 AWS EC2에 Java 배포 방법 소개/script/BeforInstall.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+sudo mkdir -vp /spring-boot-helloworld/release
+
 TM=$(date +%F_%T)
 
 # 이전에 실행되었던 jar 파일을 백업 
-# mv /spring-boot-helloworld/*.jar /spring-boot-helloworld/$TM.jar
+if [ -e /spring-boot-helloworld/*.jar ]; then
+    mv /spring-boot-helloworld/*.jar /spring-boot-helloworld/$TM.jar
+fi
